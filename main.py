6cbdc0d2703a373915d823e79641c2bd0a6b2176
argana <arganapulungan@gmail.com>
2024-02-12 12:26:15 +0700

    finish

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ st.set_page_config(
     layout= "wide"
 )
 
-"""-Dataset"""
+#title
+with st.container(border=True):
+    st.title("Analisis Statement di Twitter Mengenai Pemilihan anggota parlemen di India")
+    st.subheader("oleh Ilham azrinargana Pulungan")
 
 # #Load dataset
 # from google.colab import files
@@ -21,106 +24,114 @@ st.set_page_config(
 
 #dataset
 
-path_dataset = st.secrets.path_configuration.path_dataset
-st.text(path_dataset)
-# filename = "Twitter_Data.csv"
-# twitter = pd.DataFrame(f"{path_dataset}{filename}")
+# path_dataset = st.secrets.path_configuration.path_dataset
+# st.text(path_dataset)
+with st.container(border=True):
+    st.write("Dataset", align='center')
+filename = "Twitter_Data.csv"
+twitter = pd.read_csv(filename)
+twitter.rename(columns={'clean_text': 'text'}, inplace=True)
+twitter.drop(labels=['category'],axis=1,inplace=True)
+st.dataframe(twitter,
+             use_container_width=True,
+             hide_index=True
+             )
 
+def cleanTxt(text):
+    if isinstance(text, str):
+        # Remove mentions
+        text = re.sub(r'@[A-Za-z0-9_]+', '', text)
+        # Remove special characters and numbers
+        text = re.sub(r'[^a-zA-Z]', ' ', text)
+        # Remove single characters
+        text = re.sub(r'\s+[a-zA-Z]\s+', ' ', text)
+        # Remove multiple spaces
+        text = re.sub(r'\s+', ' ', text)
+        return text.strip()
+    else:
+        return text  # Return non-string values unchanged
 
-# """-Clean  text"""
+# Assuming twitter is your DataFrame
+twitter['text'] = twitter['text'].apply(cleanTxt)
 
-# def cleanTxt(text):
-#     if isinstance(text, str):
-#         # Remove mentions
-#         text = re.sub(r'@[A-Za-z0-9_]+', '', text)
-#         # Remove special characters and numbers
-#         text = re.sub(r'[^a-zA-Z]', ' ', text)
-#         # Remove single characters
-#         text = re.sub(r'\s+[a-zA-Z]\s+', ' ', text)
-#         # Remove multiple spaces
-#         text = re.sub(r'\s+', ' ', text)
-#         return text.strip()
-#     else:
-#         return text  # Return non-string values unchanged
+with st.container(border=True):
+    st.write("Subjectivity and polarity", align='center')
 
-# # Assuming twitter is your DataFrame
-# twitter['text'] = twitter['text'].apply(cleanTxt)
+def getSubjectivity(text):
+    if isinstance(text, str):
+        return TextBlob(text).sentiment.subjectivity
+    else:
+        return None
 
-# # Display the modified DataFrame
-# display(twitter)
+def getPolarity(text):
+    if isinstance(text, str):
+        return TextBlob(text).sentiment.polarity
+    else:
+        return None
 
-# def getSubjectivity(text):
-#     if isinstance(text, str):
-#         return TextBlob(text).sentiment.subjectivity
-#     else:
-#         return None
+twitter['Subjectivity'] = twitter['text'].apply(getSubjectivity)
+twitter['polarity'] = twitter['text'].apply(getPolarity)
+st.dataframe(twitter,
+             use_container_width=True,
+             hide_index=True
+             )
 
-# def getPolarity(text):
-#     if isinstance(text, str):
-#         return TextBlob(text).sentiment.polarity
-#     else:
-#         return None
+with st.container(border=True):
+    st.write("WordCloud", align='center')
 
-# twitter['Subjectivity'] = twitter['text'].apply(getSubjectivity)
-# twitter['polarity'] = twitter['text'].apply(getPolarity)
+allwords = ''.join([str(twts) for twts in twitter['text'] if isinstance(twts, str)])
 
-# display(twitter)
+wordcloud = WordCloud(width=500, height=300, random_state=21, max_font_size=119).generate(allwords)
 
-# """-Word CLoud"""
+st.image(wordcloud.to_array(), caption='WordCloud')
 
-# allwords = ''.join([str(twts) for twts in twitter['text'] if isinstance(twts, str)])
+with st.container(border=True):
+    st.write("Analysis", align='center')
+def getAnalysis(score):
+  if score <0:
+    return 'Negative'
+  elif score == 0:
+    return 'Netral'
+  else:
+    return 'positive'
 
-# wordcloud = WordCloud(width=500, height=300, random_state=21, max_font_size=119).generate(allwords)
+twitter['analysis'] = twitter['polarity'].apply(getAnalysis)
 
-# plt.imshow(wordcloud, interpolation="bilinear")
-# plt.axis('off')
+st.dataframe(twitter,
+             use_container_width=True,
+             hide_index=True
+             )
 
-# """-Subjectivity and Polarity"""
+with st.container(border=True):
+    st.write("Positive Statement", align='center')
 
-# def getAnalysis(score):
-#   if score <0:
-#     return 'Negative'
-#   elif score == 0:
-#     return 'Netral'
-#   else:
-#     return 'positive'
+j = 1
+sortedTwitter = twitter.sort_values(by=['polarity'])
 
-# twitter['analysis'] = twitter['polarity'].apply(getAnalysis)
+for i in range(0, sortedTwitter.shape[0]):
+    if sortedTwitter['polarity'][i] > 0 and j <= 10:  # Mengecek apakah sentimen positif
+        st.write(str(j) + ') ' + sortedTwitter['text'][i])
+        st.write()
+        j = j + 1
 
-# display(twitter)
+with st.container(border=True):
+    st.write("Negative Statement", align='center')
 
-# """-Positive Statement"""
+j = 1
+sortedTwitter = twitter.sort_values(by=['polarity'])
 
-# j = 1
-# sortedTwitter = twitter.sort_values(by=['polarity'])
+for i in range(0, sortedTwitter.shape[0]):
+    if sortedTwitter['polarity'][i] < 0 and j <= 10:  # Mengecek apakah sentimen positif
+        st.write(str(j) + ') ' + sortedTwitter['text'][i])
+        st.write()
+        j = j + 1
 
-# for i in range(0, sortedTwitter.shape[0]):
-#     if sortedTwitter['polarity'][i] > 0:  # Mengecek apakah sentimen positif
-#         print(str(j) + ') ' + sortedTwitter['text'][i])
-#         print()
-#         j = j + 1
+with st.container(border=True):
+    st.write("Sentiment analysis Result", align='center')
 
-# """
-
-# -Negative Statement
-
-# """
-
-# j = 1
-# sortedTwitter = twitter.sort_values(by=['polarity'])
-
-# for i in range(0, sortedTwitter.shape[0]):
-#     if sortedTwitter['polarity'][i] < 0:  # Mengecek apakah sentimen positif
-#         print(str(j) + ') ' + sortedTwitter['text'][i])
-#         print()
-#         j = j + 1
-
-# """-Sentiment analisis"""
-
-# twitter['analysis'].value_counts()
-
-# plt.title('sentiment Analysis')
-# plt.xlabel('sentiment')
-# plt.ylabel('counts')
-# twitter['analysis'].value_counts().plot(kind='bar')
-# plt.show()
+twitter['analysis'].value_counts()
+plt.figure(figsize=(10, 6))
+plt.title('sentiment Analysis')
+plt.xlabel('sentiment')
+plt.ylabel('counts')
+st.bar_chart(twitter['analysis'].value_counts())
